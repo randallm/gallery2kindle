c48f1efa6d06d02efb8e7d93df9fb65ac8eda93c
Randall Ma <randall@manbearpig.(none)>
2012-06-12 17:56:42 -0700

    Gave script argument options, cleaned up code, added usage example to README, supports more than 1 file format

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 gallery2kindle
 ==============
 Python script that takes Imgur galleries and embeds them into blank web pages optimally formatted for kindle web browser
+
+Usage:
+
+gallery2kindle.py [-k Kindle version] [-n name of gallery] [-d Dropbox
+directory] [Imgur Gallery URL]
+
+Example:
+
+gallery2kindle.py -k kindle3 -n Cats! [-d ~/Dropbox] http://imgur.com/a/gallery
--- gallery2kindle.py
+++ gallery2kindle.py
@@ -2,83 +2,76 @@
 
 # TODO:
 # fix code to conform to style guide (http://www.python.org/dev/peps/pep-0008/)
-# change width/height dynamically to conform with kindle web browser screen real estate
-# add bit.ly functionality
+# change width of webpage to account for kindle scrolling size
+# add bit.ly shortening functionality
 # add local image mirror functionality
-# add support for more file formats
 
 import os
 import re
 import urllib2
 import shutil
+import argparse
 
-class gallery2kindle(object):
+parser = argparse.ArgumentParser(description='Takes Imgur gallery and Kindle information; converts into Kindle-optimized gallery webpage.')
+parser.add_argument('imgur_url', metavar='URL', help='URL of Imgur gallery')
+parser.add_argument('-k', dest='kindle_type', default='3', help='Kindle model (defaults to Kindle 3); possible values include 1, 2, 3 & dx')
+parser.add_argument('-n', dest='gallery_name', default='Gallery', help='Name of new, generated gallery')
+parser.add_argument('-d', dest='dropbox_dir', help='Dropbox directory (ex: ~/Folder1/Dropbox)')
+args = vars(parser.parse_args())
 
-    def __init__(self):
-        self.numberlines = 0
-        self.numberurls = 0
+for var, value in args.items():
+    # puts all namespace entires into dictionary and uses first dictionary's keys to get new variable names; then takes first dictionary's values and uses them as new variable values 
+    globals()[var] = value
 
-    def imgurfetch(self):
-        self.imgur_url = raw_input("Imgur Gallery URL [http://imgur.com/a/gallerycode] > ")
+def imgur(imgur_url, kindle_type, gallery_name):
+    imgur_url = re.sub('\d$', '', imgur_url)
+    if imgur_url.endswith('#'):
+        imgur_url = imgur_url[:-1]
+    if '/all' not in imgur_url:
+        imgur_url = imgur_url + '/all'
+    if imgur_url.startswith('http://') == False:
+        imgur_url = 'http://' + imgur_url
 
-        # strips and formats gallery url
-        self.imgur_url = re.sub('\d$', '', self.imgur_url)
-        if self.imgur_url.endswith('#'):
-            self.imgur_url = self.imgur_url[:-1]
-        if "/all" not in self.imgur_url:
-            self.imgur_url = self.imgur_url + '/all'
-        if self.imgur_url.startswith('http://') == False:
-            self.imgur_url = 'http://' + self.imgur_url
+    imgur_url = urllib2.urlopen(imgur_url).read().splitlines()
+      
+    imgur_url = [line for line in imgur_url if line.startswith('<img alt="" src="')]
+    imgur_url = '\n'.join(imgur_url)
+    imgur_url = re.findall(r'(https?://[^\s]+)', imgur_url)
 
-        # grabs image links from gallery
-        self.imgur_url = urllib2.urlopen(self.imgur_url).read().splitlines()
+    number_urls = 0
+    while number_urls < len(imgur_url):
+        if imgur_url[number_urls].endswith('s.jpg"/>'):
+            imgur_url[number_urls] = imgur_url[number_urls][:-8] + '.jpg'
+        elif imgur_url[number_urls].endswith('s.png"/>'):
+            imgur_url[number_urls] = imgur_url[number_urls][:-8] + '.png'
+        elif imgur_url[number_urls].endswith('s.gif"/>'):
+            imgur_url[number_urls] = imgur_url[number_urls][:-8] + '.gif'
+        else:
+            pass
+        number_urls = number_urls + 1
 
-        # self.imgur_url = self.imgur_url.splitlines()
-        self.imgur_url = [line for line in self.imgur_url if '<img id="thumb--' and 's.jpg' in line] # imgur doesn't care about file extensions as long as they're valid image extensions
-        self.imgur_url = '\n'.join(self.imgur_url)
-        self.imgur_url = re.findall(r'(https?://[^\s]+)', self.imgur_url)
+    print imgur_url
 
-        # changes thumbnail links to normal links
-        while self.numberurls < len(self.imgur_url):
-            if self.imgur_url[self.numberurls].endswith('s.jpg"'):
-                self.imgur_url[self.numberurls] = self.imgur_url[self.numberurls][:-6] + '.jpg'
-            else:
-                pass
-            self.numberurls = self.numberurls + 1
+    for url in imgur_url:
+        url = url[:-7]
 
-    def imgurembed(self):
-        # Add kindle res info //UNIMPLEMENTED
+    # Add kindle res info //still need to account for 'page down' size
+    if kindle_type == '3':
+        kindle_width = '632'
+        kindle_height = '800'
 
-        self.kindletype = raw_input("Kindle Mode:\n(1) Kindle 3/4\n(2) Kindle DX\n> ")
+    final_html = '<!doctype html><title>%s - Gallery2Kindle</title><style>img{max-width:100%%;max-height:100%%;}div{width:%spx;height:%spx;}</style><div><h1>%s</h1></div>' % (gallery_name, kindle_width, kindle_height, gallery_name)
+    for url in imgur_url:
+        final_html = final_html + '<div><img src="' + url + '"></div>'
+    final_html = open(gallery_name + '.html', 'w+').write(final_html)
 
-        if self.kindletype == "1":
-            self.kindleres = "800"
+def dropbox(dropbox_dir, gallery_name):
+    dropbox_dir = os.path.expanduser(dropbox_dir)
 
-        self.galleryname = raw_input("Gallery Title:\n> ")
-        self.gallerynameheader = raw_input("Gallery Title --> Header in Web Page [yes/no]\n> ")
-        self.finalhtml = '<!doctype html>\n<html lang="en">\n<head>\n\t<meta charset="utf-8">\n\t<title>"%s - Gallery2Kindle"</title>\n\t' % self.galleryname
+    shutil.copy2(gallery_name + '.html', dropbox_dir + '/Public/' + gallery_name + '.html')
+    os.remove(gallery_name + '.html')
 
-        self.finalhtml = self.finalhtml + '<style>div{height: %spx;}</style>\n</head>\n<body>' % self.kindleres
-        for url in self.imgur_url:
-            self.finalhtml = self.finalhtml + '\n<div>\n\t<img src="' + url + '" alt="">\n</div>'
-        self.finalhtml = self.finalhtml + '\n</body>\n</html>'
-        self.htmlfile = open('gallery1.html', 'w+').write(self.finalhtml)
-
-    def dropbox(self, dropbox_dir):
-        self.maxgallery = ''.join(os.listdir(dropbox_dir))
-        self.maxgallery = re.sub('[\\D+]', '', self.maxgallery)
-	try:
-            self.maxgallery = max(list(self.maxgallery))
-            self.maxgallery = int(self.maxgallery) + 1
-            self.maxgallery = str(self.maxgallery)
-        except ValueError:
-            self.maxgallery = "1"
-        shutil.copy2('gallery1.html', dropbox_dir + '/gallery' + self.maxgallery + '.html')
-        os.remove('gallery1.html')
-
-script = gallery2kindle()
-script.imgurfetch()
-script.imgurembed()
-
-# assuming default Dropbox directory in home folder:
-script.dropbox(os.path.expanduser("~/Dropbox/Photos"))
+if __name__ == '__main__':
+    imgur(imgur_url, kindle_type, gallery_name)
+    if dropbox_dir == True:
+        dropbox(dropbox_dir, gallery_name)
